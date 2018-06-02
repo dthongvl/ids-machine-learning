0b3d96a76a973e43d2a220947721ecbb125c21bc
Thong ku <thong_pro113520@yahoo.com>
2018-06-03 05:20:13 +0700

    Enable request to predict server && edit setting

--- plugin/dpx.cc
+++ plugin/dpx.cc
@@ -60,26 +60,28 @@ size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp) {
 
 class Dpx : public Inspector {
 public:
-    Dpx(uint16_t port, uint16_t max);
+    Dpx(const std::string& host);
 
     void show(SnortConfig *) override;
 
     void eval(Packet *) override;
 
-    FeatureExtractor::IpFragment *getIpFragment(Packet *);
-
 private:
-    uint16_t port;
-    uint16_t max;
+    std::string host;
     FeatureExtractor::Config *config;
     FeatureExtractor::IpReassembler *reasm;
     FeatureExtractor::ConversationReconstructor *conv_reconstructor;
     FeatureExtractor::StatsEngine *stats_engine;
+
+    FeatureExtractor::IpFragment *getIpFragment(Packet *);
+
+    std::string request(uint8_t *buf, int size);
+
+    std::string predict(FeatureExtractor::ConversationFeatures *cf);
 };
 
-Dpx::Dpx(uint16_t p, uint16_t m) {
-    port = p;
-    max = m;
+Dpx::Dpx(const std::string& host) {
+    this->host = host;
     config = new FeatureExtractor::Config();
     stats_engine = new FeatureExtractor::StatsEngine(config);
     reasm = new FeatureExtractor::IpReassembler();
@@ -88,8 +90,7 @@ Dpx::Dpx(uint16_t p, uint16_t m) {
 
 void Dpx::show(SnortConfig *) {
     LogMessage("%s config:\n", s_name);
-    LogMessage("    port = %d\n", port);
-    LogMessage("    max = %d\n", max);
+    LogMessage("    host = %s\n", host.c_str());
 }
 
 FeatureExtractor::IpFragment *Dpx::getIpFragment(Packet *packet) {
@@ -109,7 +110,7 @@ FeatureExtractor::IpFragment *Dpx::getIpFragment(Packet *packet) {
     f->set_src_ip(packet->ptrs.ip_api.get_src()->get_ip4_value());
     f->set_dst_ip(packet->ptrs.ip_api.get_dst()->get_ip4_value());
     f->set_ip_proto((FeatureExtractor::ip_field_protocol_t) packet->ptrs.ip_api.proto());
-    f->set_ip_id((uint16_t)(packet->ptrs.ip_api.id()));
+    f->set_ip_id((uint16_t) (packet->ptrs.ip_api.id()));
     f->set_ip_flag_mf(packet->is_fragment());
     f->set_ip_frag_offset(packet->ptrs.ip_api.off());
     f->set_ip_payload_length(packet->ptrs.ip_api.pay_len());
@@ -130,7 +131,7 @@ FeatureExtractor::IpFragment *Dpx::getIpFragment(Packet *packet) {
             break;
 
         case FeatureExtractor::ICMP:
-            f->set_icmp_type((FeatureExtractor::icmp_field_type_t)packet->ptrs.icmph->type);
+            f->set_icmp_type((FeatureExtractor::icmp_field_type_t) packet->ptrs.icmph->type);
             f->set_icmp_code(packet->ptrs.icmph->code);
             break;
 
@@ -140,8 +141,93 @@ FeatureExtractor::IpFragment *Dpx::getIpFragment(Packet *packet) {
     return f;
 }
 
+std::string Dpx::request(uint8_t *buf, int size) {
+    std::string readBuffer;
+
+    curl_global_init(CURL_GLOBAL_ALL);
+    CURL *ctx = curl_easy_init();
+    curl_easy_setopt(ctx, CURLOPT_URL, host.c_str());
+
+    curl_easy_setopt(ctx, CURLOPT_POST, 1);
+    curl_easy_setopt(ctx, CURLOPT_POSTFIELDS, buf);
+    curl_easy_setopt(ctx, CURLOPT_POSTFIELDSIZE, size);
+    curl_easy_setopt(ctx, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(ctx, CURLOPT_WRITEDATA, &readBuffer);
+
+    struct curl_slist *headers = NULL;
+    headers = curl_slist_append(headers, "Content-Type: application/octet-stream");
+    curl_easy_setopt(ctx, CURLOPT_HTTPHEADER, headers);
+
+    CURLcode ret = curl_easy_perform(ctx);
+    if (ret != CURLE_OK)
+        fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                curl_easy_strerror(ret));
+
+    curl_slist_free_all(headers);
+    curl_easy_cleanup(ctx);
+
+    return readBuffer;
+}
+
+std::string Dpx::predict(FeatureExtractor::ConversationFeatures *cf) {
+    flatbuffers::FlatBufferBuilder builder(1024);
+    auto protocolType = builder.CreateString(cf->get_conversation()->get_protocol_type_str());
+    auto service = builder.CreateString(cf->get_conversation()->get_service_str());
+    auto flag = builder.CreateString(cf->get_conversation()->get_state_str());
+
+    kmeans::PacketBuilder packetBuilder(builder);
+    packetBuilder.add_duration(cf->get_conversation()->get_duration_ms() / 1000);
+    packetBuilder.add_protocol_type(protocolType);
+    packetBuilder.add_service(service);
+    packetBuilder.add_flag(flag);
+    packetBuilder.add_src_bytes(cf->get_conversation()->get_src_bytes());
+    packetBuilder.add_dst_bytes(cf->get_conversation()->get_dst_bytes());
+    packetBuilder.add_land(cf->get_conversation()->land());
+    packetBuilder.add_wrong_fragment(cf->get_conversation()->get_wrong_fragments());
+    packetBuilder.add_urgent(cf->get_conversation()->get_urgent_packets());
+    packetBuilder.add_hot(0);
+    packetBuilder.add_num_failed_logins(0);
+    packetBuilder.add_logged_in(0);
+    packetBuilder.add_num_compromised(0);
+    packetBuilder.add_root_shell(0);
+    packetBuilder.add_su_attempted(0);
+    packetBuilder.add_num_root(0);
+    packetBuilder.add_num_file_creations(0);
+    packetBuilder.add_num_shells(0);
+    packetBuilder.add_num_access_files(0);
+    packetBuilder.add_num_outbound_cmds(0);
+    packetBuilder.add_is_host_login(0);
+    packetBuilder.add_is_guest_login(0);
+    packetBuilder.add_count(cf->get_count());
+    packetBuilder.add_srv_count(cf->get_srv_count());
+    packetBuilder.add_serror_rate(cf->get_serror_rate());
+    packetBuilder.add_srv_serror_rate(cf->get_srv_serror_rate());
+    packetBuilder.add_rerror_rate(cf->get_rerror_rate());
+    packetBuilder.add_srv_rerror_rate(cf->get_srv_rerror_rate());
+    packetBuilder.add_same_srv_rate(cf->get_same_srv_rate());
+    packetBuilder.add_diff_srv_rate(cf->get_diff_srv_rate());
+    packetBuilder.add_srv_diff_host_rate(cf->get_srv_diff_host_rate());
+    packetBuilder.add_dst_host_count(cf->get_dst_host_count());
+    packetBuilder.add_dst_host_srv_count(cf->get_dst_host_srv_count());
+    packetBuilder.add_dst_host_same_srv_rate(cf->get_dst_host_same_srv_rate());
+    packetBuilder.add_dst_host_diff_srv_rate(cf->get_dst_host_diff_srv_rate());
+    packetBuilder.add_dst_host_same_src_port_rate(cf->get_dst_host_same_src_port_rate());
+    packetBuilder.add_dst_host_srv_diff_host_rate(cf->get_dst_host_srv_diff_host_rate());
+    packetBuilder.add_dst_host_serror_rate(cf->get_dst_host_serror_rate());
+    packetBuilder.add_dst_host_srv_serror_rate(cf->get_dst_host_srv_serror_rate());
+    packetBuilder.add_dst_host_rerror_rate(cf->get_dst_host_rerror_rate());
+    packetBuilder.add_dst_host_srv_rerror_rate(cf->get_dst_host_srv_rerror_rate());
+    auto orc = packetBuilder.Finish();
+    builder.Finish(orc);
+    uint8_t *buf = builder.GetBufferPointer();
+    int size = builder.GetSize();
+
+    std::string result = request(buf, size);
+    return result;
+}
+
 void Dpx::eval(Packet *packet) {
-    if (packet->is_ip4()  && (packet->is_tcp() || packet->is_udp() || packet->is_icmp())) {
+    if (packet->is_ip4() && (packet->is_tcp() || packet->is_udp() || packet->is_icmp())) {
         FeatureExtractor::IpFragment *frag = getIpFragment(packet);
         FeatureExtractor::Packet *datagr = nullptr;
 
@@ -160,92 +246,9 @@ void Dpx::eval(Packet *packet) {
         FeatureExtractor::ConversationFeatures *cf = stats_engine->calculate_features(conv);
         conv = nullptr;        // Should not be used anymore, object will commit suicide
 
-        cf->print(config->should_print_extra_features());
+        std::cout << predict(cf) << std::endl;
         delete cf;
     }
-//    flatbuffers::FlatBufferBuilder builder(1024);
-//    auto protocolType = builder.CreateString("tcp");
-//    auto service = builder.CreateString("private");
-//    auto flag = builder.CreateString("REJ");
-//
-//    kmeans::PacketBuilder packetBuilder(builder);
-//    packetBuilder.add_duration(0);
-//    packetBuilder.add_protocol_type(protocolType);
-//    packetBuilder.add_service(service);
-//    packetBuilder.add_flag(flag);
-//    packetBuilder.add_src_bytes(0);
-//    packetBuilder.add_dst_bytes(0);
-//    packetBuilder.add_land(0);
-//    packetBuilder.add_wrong_fragment(0);
-//    packetBuilder.add_urgent(0);
-//    packetBuilder.add_hot(0);
-//    packetBuilder.add_num_failed_logins(0);
-//    packetBuilder.add_logged_in(0);
-//    packetBuilder.add_num_compromised(0);
-//    packetBuilder.add_root_shell(0);
-//    packetBuilder.add_su_attempted(0);
-//    packetBuilder.add_num_root(0);
-//    packetBuilder.add_num_file_creations(0);
-//    packetBuilder.add_num_shells(0);
-//    packetBuilder.add_num_access_files(0);
-//    packetBuilder.add_num_outbound_cmds(0);
-//    packetBuilder.add_is_host_login(0);
-//    packetBuilder.add_is_guest_login(0);
-//    packetBuilder.add_count(229);
-//    packetBuilder.add_srv_count(10);
-//    packetBuilder.add_serror_rate(0);
-//    packetBuilder.add_srv_serror_rate(0);
-//    packetBuilder.add_rerror_rate(1);
-//    packetBuilder.add_srv_rerror_rate(1);
-//    packetBuilder.add_same_srv_rate(0.04);
-//    packetBuilder.add_diff_srv_rate(0.06);
-//    packetBuilder.add_srv_diff_host_rate(0);
-//    packetBuilder.add_dst_host_count(255);
-//    packetBuilder.add_dst_host_srv_count(10);
-//    packetBuilder.add_dst_host_same_srv_rate(0.04);
-//    packetBuilder.add_dst_host_diff_srv_rate(0.06);
-//    packetBuilder.add_dst_host_same_src_port_rate(0);
-//    packetBuilder.add_dst_host_srv_diff_host_rate(0);
-//    packetBuilder.add_dst_host_serror_rate(0);
-//    packetBuilder.add_dst_host_srv_serror_rate(0);
-//    packetBuilder.add_dst_host_rerror_rate(1);
-//    packetBuilder.add_dst_host_srv_rerror_rate(1);
-//    auto orc = packetBuilder.Finish();
-//    builder.Finish(orc);
-//    uint8_t *buf = builder.GetBufferPointer();
-//    int size = builder.GetSize();
-//
-//    std::string readBuffer;
-//
-//    curl_global_init(CURL_GLOBAL_ALL);
-//    CURL *ctx = curl_easy_init();
-//    curl_easy_setopt(ctx, CURLOPT_URL, "http://localhost:5000/predict");
-//
-//    curl_easy_setopt(ctx, CURLOPT_POST, 1);
-//    curl_easy_setopt(ctx, CURLOPT_POSTFIELDS, buf);
-//    curl_easy_setopt(ctx, CURLOPT_POSTFIELDSIZE, size);
-//    curl_easy_setopt(ctx, CURLOPT_WRITEFUNCTION, WriteCallback);
-//    curl_easy_setopt(ctx, CURLOPT_WRITEDATA, &readBuffer);
-//
-//    struct curl_slist *headers = NULL;
-//    headers = curl_slist_append(headers, "Content-Type: application/octet-stream");
-//    curl_easy_setopt(ctx, CURLOPT_HTTPHEADER, headers);
-//
-//    CURLcode ret = curl_easy_perform(ctx);
-//    if (ret != CURLE_OK)
-//        fprintf(stderr, "curl_easy_perform() failed: %s\n",
-//                curl_easy_strerror(ret));
-//
-//    curl_slist_free_all(headers);
-//    curl_easy_cleanup(ctx);
-//
-//    std::cout << readBuffer << std::endl;
-    // precondition - what we registered for
-    //    assert(p->is_udp());
-    //
-    //    if ( p->ptrs.dp == port && p->dsize > max )
-    //        DetectionEngine::queue_event(DPX_GID, DPX_SID);
-
 }
 
 //-------------------------------------------------------------------------
@@ -254,11 +257,8 @@ void Dpx::eval(Packet *packet) {
 
 static const Parameter dpx_params[] =
         {
-                {"port",  Parameter::PT_PORT, nullptr, nullptr,
-                        "port to check"},
-
-                {"max",   Parameter::PT_INT, "0:65535", "0",
-                        "maximum payload before alert"},
+                {"host",  Parameter::PT_STRING, nullptr, nullptr,
+                        "predict server to connect"},
 
                 {nullptr, Parameter::PT_MAX,  nullptr, nullptr, nullptr}};
 
@@ -286,17 +286,12 @@ public:
     Usage get_usage() const override { return INSPECT; }
 
 public:
-    uint16_t port;
-    uint16_t max;
+    std::string host;
 };
 
 bool DpxModule::set(const char *, Value &v, SnortConfig *) {
-    if (v.is("port"))
-        port = v.get_long();
-
-    else if (v.is("max"))
-        max = v.get_long();
-
+    if (v.is("host"))
+        host = v.get_string();
     else
         return false;
 
@@ -313,7 +308,7 @@ static void mod_dtor(Module *m) { delete m; }
 
 static Inspector *dpx_ctor(Module *m) {
     DpxModule *mod = (DpxModule *) m;
-    return new Dpx(mod->port, mod->max);
+    return new Dpx(mod->host);
 }
 
 static void dpx_dtor(Inspector *p) {
